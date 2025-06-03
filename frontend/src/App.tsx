deef6bae085c0bffef1c4bc8c422ced01f3e214e
nikusha09 <nikusha.bakhtadze123@gmail.com>
2025-06-03 18:57:27 +0400

    listen for ball position, score updates, and game over events on client(basic structure)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
+import React from 'react';
+import Game from './components/Game';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <></>
-  )
+    <div>
+      <h1>Real-Time Pong</h1>
+      <Game />
+    </div>
+  );
 }
 
-export default App
+export default App;
