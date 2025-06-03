8788bce1af5f4a79532321dd7efaabcf1d719a1b
nikusha09 <nikusha.bakhtadze123@gmail.com>
2025-06-03 19:12:41 +0400

    add types/interfaces

---
+++ backend/src/types.ts
@@ -0,0 +1,20 @@
+export interface Player {
+    id: string;
+    paddleY: number;
+    score: number;
+}
+
+export interface GameState {
+    ballX: number;
+    ballY: number;
+    ballVelocityX: number;
+    ballVelocityY: number;
+    players: { [playerId: string]: Player };
+}
+
+export interface Room {
+    name: string;
+    players: string[];
+    gameState: GameState;
+    interval?: NodeJS.Timer;
+}
